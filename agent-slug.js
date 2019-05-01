0414ff41d139880465f1109b4b61ad0c48eb1c6e
Brent Hackman <brenthack35@gmail.com>
2019-05-01 14:19:14 -0400

    change

--- agent-slug.js
+++ agent-slug.js
@@ -1,4 +1,4 @@
-// Parse Agent Slug from URL
+// Parse User Slug from URL
 function getParameterByName(name) {
 	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   var regex = new RegExp('[\\?&]' + name + '=([^&#]*)', 'i'),
@@ -7,46 +7,26 @@ function getParameterByName(name) {
 }
 
 // Give the URL parameter variable a name
-var agentSlug = getParameterByName('agent');
+var userSlug = getParameterByName('user');
 
-// Set the Cookie (agent slug)
-if (agentSlug) {
-  $.cookie('agent', agentSlug);
+// Set the Cookie (user slug)
+if (userSlug) {
+  $.cookie('user', userSlug);
 }
 
 $(document).ready(function(){
 
-    // Grab the Cookie (agent slug) and set the Medicareful URLs with the slug
-    if ($.cookie('agent')) {
-      if ($.cookie('agent') != 'medicareful') {
-        $('a[href^="https://medicareful.com"], a[href^="http://medicareful.com"], a[href^="https://www.medicareful.com"], a[href^="http://www.medicareful.com"]').each(function(){
-          this.href="https://medicareful.com/" + $.cookie("agent");
+    // Grab the Cookie (user slug) and set URLs with the slug
+    if ($.cookie('user')) {
+      if ($.cookie('user') != 'mydomain') {
+        $('a[href^="https://mydomain.com"], a[href^="http://mydomain.com"], a[href^="https://www.mydomain.com"], a[href^="http://www.mydomain.com"]').each(function(){
+          this.href="https://mydomain.com/" + $.cookie("user");
         });
       }
       updateURL();
     }
 
-    $('.navbar .container').animate({opacity: 1}, 800);
-
-    // Show Content Search on Click
-    $('.fa-search').on('click', function() {
-      $('.search-modal').modal('show');
-    });
-
-    // Scroll to top of search result when paging
-    $('.search-modal').on('shown.bs.modal', function() {
-      $('.modal-dialog').on('click', '.page-link', function(event) {
-        setTimeout(function(){
-          $('.modal-dialog').animate({
-              scrollTop: 0
-          }, 600);
-          return false;
-        }, 800 );
-      });
-    });
-});
-
-// append Cookie (agent slug) to end of URL
+// append Cookie (user slug) to end of URL
 function updateURL() {
   if (history.pushState) {
       var newurl = window.location.origin + window.location.pathname;
